e18bf6e734359989172995f73e48f0de8a463334
MIVI <mickael.vilalard@hotmail.fr>
2020-03-31 14:28:54 +0200

    message

---
+++ spark-work-count/src/main/scala/xke/local/HelloWorld.scala
@@ -0,0 +1,22 @@
+package xke.local
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("test").master("local[*]").getOrCreate()
+
+    // code
+    // src/main/resources/departements-france.csv
+    // 1) lire le fichier
+    // 2) créer une colonne avec la moyenne des numéro département par code région
+    //    code_region, avg_dep, nom_region
+    // 3) renommer la colonne moyenne des départements en avg_dep
+    // 4) écrire le fichier en parquet
+
+  }
+
+  def avgDepByReg: DataFrame = ???
+  def renameColumn: DataFrame = ???
+}
